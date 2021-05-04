815674df9d2ca050fdfd4f6a1c09ae73a9e5ab56
hobob123 <73672291+hobob123@users.noreply.github.com>
2021-05-03 22:56:43 -0500

    Update default.py

--- default.py
+++ default.py
@@ -1,9 +1,13 @@
+CODE_TYPE_LIST = ["Python GUI", "KODI GUI", "Terminal"]
+Code_type = CODE_TYPE_LIST[1]
+
 ##------------------------------------------------------------------------------------
 #                              KODI Imports
 ##------------------------------------------------------------------------------------
-import xmbc
-import xbmcaddon
-import xbmcgui
+if Code_type == CODE_TYPE_LIST[1]:
+    import xbmc
+    import xbmcaddon
+    import xbmcgui
 
 
 ##-----------------------------------------------------------------------------------
@@ -28,25 +32,18 @@ import cec
 #Library from here- https://github.com/trainman419/python-cec
 
 
-#Used by the HUD Class
-from PIL import Image, ImageDraw
-# Credits for this ^^^^^^^^^^^^^^^^^^^^^^^^^^^
-# https://code-maven.com/create-images-with-python-pil-pillow
-
-
 #Used for GUI without KODI
-import easygui
+if (Code_type == CODE_TYPE_LIST[0]):
+    import easygui
+
 # Credits for this ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Library from - https://easygui.readthedocs.io/en/master/api.html
 
-CODE_TYPE_LIST = ["Python GUI", "KODI GUI", "Terminal"]
-
-Code_type = CODE_TYPE_LIST[1]
 
 
 #Important Classes
 
-#HUD Controls (Used to turn the TV on and off)
+#TVControls (Used to turn the TV on and off)
 class TV_CEC:
 
     def __init__(self):
@@ -94,85 +91,6 @@ class TV_CEC:
         self.change_mute_status()
         self.change_mute_status()
 
-#HUD Controls (Used to generate images to show the user)
-class HUD_Pictures:
-    def __init__(self, distance = 0, rpm = 0.0, mph = 0.0, time_biked = [0, 0, 0], name = "-1"):
-        """
-        Initialization
-        No inputs required.
-        Name should be initialized here
-        """
-        self.distance = distance
-
-        self.rpm = rpm
-
-        self.mph = mph
-
-        self.time_biked = time_biked
-
-        self.name = name
-
-        self.stats = Image.new("RGB", (500, 100), color = 0)
-
-        self.stats.save("Stats.png")
-
-        self.warning = Image.new("RGB", (500, 100), color = (255, 0 , 0))
-
-        self.warning.save("Warning.png")
-
-##-----------------------------------------------------------------------------------
-#   Kodi doesn't constantly update skin images, so this hasn't been incorporated yet
-##-----------------------------------------------------------------------------------
-    def createStatsImage(self, debug = False):
-        """
-        Shows the statistics in a nice little rectangle that can go at the top right of the screen
-        """
-        self.stats = Image.new("RGB", (500, 100), color = 0)
-        d = ImageDraw.Draw(self.stats)
-
-        if self.name == "-1":
-            d.text((10,5), "Here are your biking stats:", fill=(255,255,0))
-
-        else:
-            d.text((10,5), "Here are your biking stats " + self.name + ":", fill=(255,255,0))
-
-        #d.text((10,25), "You have traveled " + str(self.distance) + " miles", fill = (255,255,0))
-
-        d.text((10,45), "Wheel RPM: " + str(self.rpm), fill = (255,255,0))
-
-        d.text((10,65), "MPH: " + str(self.mph), fill = (255,255,0))
-
-        d.text((10,85), "You have been biking for " + str(self.time_biked[0]) + " hours "  + str(self.time_biked[1]) + " minutes, and " + str(round(self.time_biked[2])) + " seconds", fill = (255,255,0))
-
-        self.stats.save("/home/pi/Desktop/ICSoftware/Stats.jpeg")
-        
-        self.stats.save("/home/pi/.kodi/addons/skin.apptv/media/HUD.jpeg")
-        
-        if debug == True:
-            self.stats.show()
-
-    def createWarningImage(self, time_remaining = [0, 0], debug = False):
-        """
-        Input: A List showing how much time is left
-
-        Shows a warnign telling the user how long they have until their TV turns off
-        """
-        self.warning = Image.new("RGB", (500, 100), color = (255, 0 , 0))
-        draw = ImageDraw.Draw(self.warning)
-
-        draw.text((100,45), "Shutting down in: " + str(time_remaining[0]) + " minutes and " + str (time_remaining[1]) + " seconds", fill = (255,255,255))
-        draw.text((125,65), "Start biking in order to unmute TV")
-        
-        self.warning = self.warning.resize((1000,200), Image.ANTIALIAS)
-        
-        self.warning.save("/home/pi/Desktop/ICSoftware/Warning.jpeg")
-        
-        self.warning.save("/home/pi/.kodi/addons/skin.apptv/media/HUD.jpeg")
-        
-        if debug == True:
-            self.warning.show()
-
-
 #Important Functions:
 def get_now_time():
         """
@@ -199,24 +117,22 @@ def miles_per_hour(rpm, wheel_diameter = 26):
     Takes a rpm value (int or float)
     default wheel = 26, but can be changed
 
-    returns float of 
+    returns float of
     """
-        IN_Per_MIN = rpm * wheel_diameter * 3.1415
+    IN_Per_MIN = rpm * wheel_diameter * 3.1415
 
-        IN_Per_HR = IN_Per_MIN * 60
+    IN_Per_HR = IN_Per_MIN * 60
 
-        mph = IN_Per_HR / 63360
+    mph = IN_Per_HR / 63360
 
-        return mph
+    return mph
 
 ##-----------------------------------------------------------------------------------
 #              Initializes objects and variables used in the Main loop
 ##-----------------------------------------------------------------------------------
 
 TV = TV_CEC()
-
-HUD = HUD_Pictures()
-
+KODIDialog = xbmcgui.Dialog()
 
 #Variables
 flag = False
@@ -255,7 +171,7 @@ if Code_type == "Python GUI":
 
 #Needs development
 if Code_type == "KODI GUI":
-    threshold = 10
+    threshold = int(KODIDialog.select("Chose an RPM Threshold", ["5", "10", "15", "20"]))
     name = "-1"
 
 
@@ -297,12 +213,6 @@ ADDON = xbmcaddon.Addon()
 path = ADDON.getAddonInfo('path').decode('utf-8')
 
 
-warning5min = xbmcgui.WindowXML("Custom_1198_5min.xml", path, defaultRes = "1080i", isMedia = True)
-
-warning1min = xbmcgui.WindowXML("Custom_1197_1min.xml", path, defaultRes = "1080i", isMedia = True)
-
-warning5sec = xbmcgui.WindowXML("Custom_1199_5sec.xml", path, defaultRes = "1080i", isMedia = True)
-
 ##------------------------------------------------------------------------------------
 #                                Main loop
 ##------------------------------------------------------------------------------------
@@ -313,15 +223,8 @@ warning5sec = xbmcgui.WindowXML("Custom_1199_5sec.xml", path, defaultRes = "1080
 time_Started = get_now_time()
 
 while (__name__ == "__main__"):
-    warning5min = xbmcgui.WindowXML("Custom_1198_5min.xml", path, defaultRes = "1080i", isMedia = True)
-
-    warning1min = xbmcgui.WindowXML("Custom_1197_1min.xml", path, defaultRes = "1080i", isMedia = True)
-
-    warning5sec = xbmcgui.WindowXML("Custom_1199_5sec.xml", path, defaultRes = "1080i", isMedia = True)
-
 
     #Does the RPM from the GPIO thing
-    lastrpm = rpm
 
     if (GPIO.input(MAGNET_GPIO) == 0):
         if (flag == False):
@@ -341,7 +244,7 @@ while (__name__ == "__main__"):
 
             rpm = 60/dtime
 
-    rpm = (rpm + lastrpm)/2
+
 
     print (rpm)
 
@@ -350,45 +253,14 @@ while (__name__ == "__main__"):
         print("Low RPM")
         TVFlag = True
         listime = [5,0]
-        timeremaining = listime.copy()
+        timeremaining = listime[:]
         #loop for the timer
         for i in range (listime[0]*60 + listime[1]):
-            
 
-            #This is the display for the warning images
-            if ((timeremaining[0] == 5) or (timeremaining[0] == 4)  or (timeremaining[0] == 3) or (timeremaining[0] == 2)):
-                warning5min.doModal()
-
-            else:
-                try:
-                    del warning5min
-                except:
-                    pass
-
-            
-            if (timeremaining[0] == 1):
-                warning1min.doModal()
-
-            else:
-                try:
-                    del warning1min
-                except:
-                    pass
-
-            
-            if ((timeremaining[0] == 0) and (timeremaining[1] <= 5)):
-                warning5sec,doModal()
-
-            else:
-                try:
-                    del warning5sec
-                except:
-                    pass
-
+            KODIDialog.yesno("Time Remaining", "You have " + str(timeremaining[0]) + " Minutes and " + str(timeremaining[1]) + " Seconds until the TV turns off! Start biking!", autoclose = 750)
 
             print(timeremaining)
             #creates a warning image
-            HUD.createWarningImage(time_remaining = timeremaining)
 
             #does some stuff that allows the timer to be displayed in the warning
             if timeremaining[1] == 0:
@@ -421,7 +293,7 @@ while (__name__ == "__main__"):
                     dtime = dtimelis[0]*3600 + dtimelis[1]*60 + dtimelis[2]
 
                     rpm = 60/dtime
-            rpm = (rpm + lastrpm)/2
+
 
 
             if rpm > threshold:
@@ -439,11 +311,7 @@ while (__name__ == "__main__"):
                                 current_time[2] - time_Started[2]]
 
 
-                HUD.rpm = rpm
-                HUD.mph = miles_per_hour(rpm)
-                HUD.time_biked = time_elapsed
-                HUD.name = name
-                HUD.createStatsImage()
+
                 break
 
             time.sleep(1)
@@ -467,11 +335,7 @@ while (__name__ == "__main__"):
                         current_time[2] - time_Started[2]]
 
 
-        HUD.rpm = rpm
-        HUD.mph = miles_per_hour(rpm)
-        HUD.time_biked = time_elapsed
-        HUD.name = name
-        HUD.createStatsImage()
+
 
 
     #Sleeps in order for the RPM sensing to work
